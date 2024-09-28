41611d5bf6111150b17252ff64401e02e6a095a2
nika19914 <nikakakhadze48@gmail.com>
2024-09-28 16:28:54 +0400

    nika

---
+++ day 00/lesson0.py
@@ -0,0 +1,39 @@
+name = "nika"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმანიჭებელი სიმბოლო
+#"nika" არის ცვალადისთვის მნიშვნელობა
+
+surname = "kakhadze"
+
+#print(name)
+# პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "nika" # ეს არის str (string) ტიპის ცვლადი
+age = 14 # ეს არის int ( integer ) მთელი რიცხვი
+height = 1.74 # ეს არის float ტიპის ცვლადი ( ათწილადი )
+#boolean (bool) ტიპის ცვლადი
+
+knows_programing = True #ture ან false
+is_ugly = False #snakecase ( უბრალოდ წერის სტილის სტანდარტულად )
+
+isUgly = False # ჯავასკრიპტული camelcase
+
+
+#print(name + "  " + surname)  
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name + age)
+
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programing))
+
+print(name + " " +str(age))
+name ="me var nika"
+surname ="kakhadze"
+age = 14
+height = 1.74
+
+print(name + " " + surname + " " +str(age) + " " +str(height))
