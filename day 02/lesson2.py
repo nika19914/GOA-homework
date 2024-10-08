bbb4a425e65210ed1d08fefd973173ec2bc53e26
nika19914 <nikakakhadze48@gmail.com>
2024-10-08 20:58:06 +0400

    nika kakhade

---
+++ day 02/lesson2.py
@@ -0,0 +1,22 @@
+print(" nika kakhadze ")
+print("xatuna paksadze")
+print("tite kakhadze")
+print("kakhi kakhadze")
+print("saba davitadze")
+print("lika kakhadze")
+
+
+
+print(5 + 5)
+print(13 + 6 - 8)
+print(5+7+9+78+34-59)
+print((45+78)-(98-89))
+print(1900+(569-400))
+print(54576 - 5)
+print(1367 - 69 - 8)
+print(5+7+9+78+34-59)
+print((4+5+7-8)*(95-89))
+print(74623+(769-400))
+print(4*37684)
+((2376456) * (137465))
+
