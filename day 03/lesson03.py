31158ebb61af26c5b5517df05745e72f7f5009cb
nika19914 <nikakakhadze48@gmail.com>
2024-10-11 17:50:08 +0400

    nika kakhadze

---
+++ day 03/lesson03.py
@@ -0,0 +1,99 @@
+x=49
+y=32
+print(x * y)
+
+num1=6352
+num2=245
+num3=13245
+print(num1 + num2 + num3)
+
+
+phrase="Goa is the best academy in the world"
+times=99
+print(phrase * times)
+
+
+x=78
+y=687
+print(x * y)
+
+
+
+name="giga"
+surname="cecxladze"
+print(name+ surname)
+
+height=1.74
+weight=100
+print(height + weight)
+
+
+bankcode=4567
+phonecode=123456
+print(bankcode+phonecode)
+
+
+sisname="lika"
+broname="gio"
+print(sisname+broname)
+
+
+
+
+
+
+
+#ცვლადის სახელის მოფიქრება
+# უდრის, მინიჭების ოპერატორის დაწერა
+# მნიშვნელობის მინიჭება
+
+
+#დავალება4
+
+
+num1=78
+num2=67
+num3=89
+print(num1 + num2 + num3)
+
+#დავალება 5
+
+
+x=45
+y=38
+print(x * y)
+
+#მართკუთხედის პერიმეტრი:დავალება 6
+
+
+length=10
+width=20
+print(length * width)
+
+#დავალება 7
+
+age1=14
+age2=16
+age3=32
+print(age1 + age2 + age3 / 3)
+
+
+#დავალება 8
+
+name= "nika"
+surname= "kakhadze"
+print(name   +   surname)
+result="nikakakhadze"
+print(result)
+
+
+#დავალება 9
+
+age=14
+fullname="nika kakhadze"
+print(age * fullname)
+#დავალება 10
+
+
+
+
