012df01dafe5d3dc3573672721701aa066deaeb9
nika19914 <nikakakhadze48@gmail.com>
2024-10-12 22:32:39 +0400

    nika kakhadze

---
+++ day 04/lesson04.py
@@ -0,0 +1,44 @@
+#დავალება 2
+
+# bug არის სერვერის ან რაიმიეს შეცდომა
+# debugging  ნიშნავს შეცდომების ამოღებას პროცესის კომპიუტერული ტექნიკიდან
+
+# დავალება 3
+
+#print(nika)
+print("nika")
+
+
+#დავალება 4
+
+
+print( 5 + 8 * 9 - 2 / 2 )
+
+
+x=89
+y=45
+print(x + y)
+print(y * x)
+
+
+num1=480
+num2=913
+print(num1 * num2)
+
+print(71625 / 78)
+
+
+nika=14
+giga=25
+print(nika * giga)
+
+#დავალება 5
+
+
+me=14
+sis=16
+mom=33
+dad= 41
+print(me + sis + mom + dad)
+
+
