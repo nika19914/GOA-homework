eb64ec235ce5d7b23cf708cfe7e5f95d5ac9c8ff
nika19914 <nikakakhadze48@gmail.com>
2024-10-13 07:28:25 +0400

    nika kakhadze

---
+++ classwork/claswork.py
@@ -0,0 +1,78 @@
+#1) შეასწორეთ (debug) შემდეგი კოდი:
+
+name = "Alice"
+print(name)
+
+
+#2
+
+salary = 75000
+age = 30
+role = "Software Engineer"
+print(salary)
+print(role)
+
+
+#შეასწორეთ კოდი:
+
+
+#3
+
+
+balance =750
+cost = 400
+print(balance - cost)
+
+#4
+
+
+num1 = 10
+num2 = 5
+print(num1 + num2)
+#String Concatenation
+
+
+#5
+
+first_name = "John"
+last_name = "Doe"
+full_name = (first_name + " " + last_name)
+print(full_name)
+#Basic Conditional Statements
+#აქ როდესაც ვპეჭდავტ ყოველთის პრინტს შჭირდება ფრჩილები
+#6
+
+num1 = 10
+num2 = 5
+print(num1 + num2)
+ #ეს სწორია
+
+ #7
+
+temperature = 30
+print(temperature)
+#აქ შეცდომა იყო ის რომ როდესაც ცვლადის დაბეჭვდა გვინდა უნდა იყოს ბრჩხილებში იმიტრო ეს არა სტრინგი
+
+
+
+#8
+
+#მომხმარებელს შემოატანინეთ მისი ასაკი და დაპრინტეთ ის, გამოიყენეთ კარგი პრაქტიკები (კომენტარები)
+
+#age= int(input"enter your age: "))
+#print(age)
+ 
+
+ #9
+ #მომხმარებელს შემოატანინეთ მისი საყვარელი ფერი და დაპრინტეთ ის, გამოიყენეთ კარგი პრაქტიკები (კომენტარები)
+#color= (input"enter your favorite color: " ))
+#print(color)
+
+
+
+#10
+
+#მომხმარებელს შემოატანინეთ მისი საყვარელი მანქანა და დაპრინტეთ ის, გამოიყენეთ კარგი პრაქტიკები (კომენტარები)
+car=(input"enter your favorite car: "))
+print(car)
+ 
