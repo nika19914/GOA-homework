8a956c2f89cafc72b1c90eff68ba4313650ac4ec
nika19914 <nikakakhadze48@gmail.com>
2024-10-13 16:43:47 +0400

    nika

---
+++ day 05/lesson5.py
@@ -0,0 +1,30 @@
+# დავალება 1
+# 1.მომხმარებელს შემოატანინეთ სახელი,გვარი,ასაკი,საცხოვრებელი ადგილი და მეილი. კომენტარებით ახსენით რას ნიშნავს თითო ხაზი ამ კოდისვ
+
+#შემოვიტანეთ სახელი
+#name=(input("enter your name: "))
+#print(name)
+
+#შემოვიტანეთ გვარი
+#surname=(input("enter your age: "))
+#print(surname)
+
+
+#შემოვიტანოთ ასაკი, სადაც გვჭირდება int- რადგან გვაინტერესებს მთლი რიცხვი
+#age=int(input("eneter you age: "))
+#print(age)
+
+#შემოვიტანოთ საცხოვრებელი ადგილი
+#adress=(input("enter your adress: "))
+#print(adress)
+
+
+#შემოგვაქ მეილი
+#email=(input("enter your email: "))
+#print(email)
+
+#დავალება 2
+#2. მომხმარებელს შემოატანინეთ ასაკი და დაუპრინტეთ რამდენი წლის იქნება ის 20 წელიწადში
+
+age_after_20_years=int(input("enter your age: "))
+print(age_after_20_years + 20)
