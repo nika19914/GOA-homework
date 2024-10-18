e1d2c30324b695c0e1eca558f78d3efcb80ab6e2
nika19914 <nikakakhadze48@gmail.com>
2024-10-18 10:36:29 +0400

    nika

---
+++ day 07/lesson7.py
@@ -0,0 +1,105 @@
+# დავალება 1
+
+#name=("nika")
+#age=(14)
+#print(type(name))
+#print(type(age))
+
+# დავალება 2
+
+#num1=int(input("enter number: "))
+#print(type(num1))
+
+# დავალება 3
+
+#print(5 > 7)
+#print( 5.7 > 90)
+#print(938657 < 5764)
+#print(89.7632145 > 89.99999999)
+#print(12 - 90 < -67897)
+#print(78631549 > 6)
+#print(34574 == 34574)
+#print( 97123465 < 80374650183745)
+#print(57.0 == 57)
+#print(45.0 + 0 == 45)
+
+
+#დავალება 4
+
+#x = 5
+#y=9
+
+#print(x > 3 and x < 10)
+#print(x > y or x > y)
+#print(x + 15 < y and y - 6 > x)
+#print(x < y or x <  y)
+#print(x == y or y == 9)
+#print(x - 20 and y < 56)
+
+#დავალება 5
+ 
+#num1=int(input(" enter number: "))
+#print(type(num1))
+
+#num1=(input(" enter number: "))
+#print(type(num1))
+
+#num1=float(input(" enter number: "))
+#print(type(num1))
+
+
+# დავალება 6
+
+#fav_num=int(input("enter your favorite number: "))
+#print(type(fav_num))
+#if fav_num % 2 == 0:
+    #print("ლუწი")
+#else :
+    #print("კენტი")
+
+
+# დავალება 7
+age=int(input("enter your age: "))
+name=input("enter you name: ")
+if name == "nika":
+    print("We have the same names")
+elif age == 1:
+    print("You are kid")
+elif age == 2:
+    print("You are kid")
+elif age == 3:
+    print("You are kid")
+elif age == 4:
+    print("You are kid")
+elif age == 5:
+    print("You are kid")
+elif age == 6:
+    print("You are kid")
+elif age == 7:
+    print("You are kid")
+elif age == 8:
+    print("You are kid")
+elif age == 9:
+    print("You are kid")
+elif age == 10:
+    print("You are kid")
+elif age == 11:
+    print("You are kid")
+elif age == 12:
+    print("You are kid")
+elif age == 13:
+    print("You are kid") 
+elif age == 14:
+    print("You are kid")
+elif age == 15:
+    print("You are kid")
+elif age == 16:
+    print("You are kid")
+elif age == 17:
+    print("You are kid")
+elif age == 18:
+    print("You are kid")
+else:
+    print("You are adult")
+
+
---
+++ idk.py
@@ -0,0 +1,23 @@
+
+num1 = 78
+
+num2 = 54
+print( num1 / num2 )
+
+print( num1 - num2 )
+print( num1 * num2 )
+
+print( num2 + num2)
+name= nika
+
+print("nika kakhadze")
+print("nika kakhadze")
+print("nika kakhadze")
+print("nika kakhadze")
+print("nika kakhadze")
+print("nika kakhadze")
+print("nika kakhadze")
+print("nika kakhadze")
+print("nika kakhadze")
+print("nika kakhadze")
+
