7fdbdb534b896ae8f087e90d382107070c8fb99c
nika19914 <nikakakhadze48@gmail.com>
2024-10-21 02:34:44 +0400

    nika

---
+++ day 09/classwork/classwork.py
@@ -0,0 +1,37 @@
+#while ციკლის საშვალებით გამოიტანეთ რიცხვები 1-იდან 10-მდე
+
+
+
+#num=0
+
+#while num < 10:
+    #num = num + 1
+    #print(num)
+
+
+#while ციკლის საშვალებით გამოიტანეთ რიცხვები 20-იდან 0-მდე
+#num = 20
+#while num >= 0:
+    #num = num - 1
+    #print(num)
+
+#while ციკლის საშვალებით გამოიტანეთ 1-დან 20-მდე მხოლოდ ლუწი რიცხვები
+
+#num1 =20
+#while num1 >=0:
+    #print(num1)
+    #num1 = num1 -2
+   
+#while ციკლის საშვალებით გამოიტანეთ 1-იდან 100-მდე ყველა 5-ის ჯერადი რიცხვი
+
+
+#num2=100
+#while num2 >=1:
+   # print(num2)
+    #num2 = num2 
+    
+    # while ციკლისა და input-ის საშვალებით მომხარებელს შემოატანინეთ პაროლი სანამ არ იქნება ის "goa123"-ის ტოლი
+real_password=("goa123")
+password=input("enter your password: ")
+while password != real_password:
+     print(password)
