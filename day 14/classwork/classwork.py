aa7cc4775c758f50fd37711b4a986244631c7ec9
nika19914 <nikakakhadze48@gmail.com>
2024-11-03 08:51:24 +0400

    nika

---
+++ day 14/classwork/classwork.py
@@ -0,0 +1,50 @@
+#1) შექმენით სია სადაც, შეიტანთ მინიმუმ 10 რიცხვს, გადაუარეთ for ციკლის დახმარებით და დაბეჭდეთ თითოეული რიცხვი კენტია თუ ლუწი.
+
+#numbers = [45, 67, 34, 66, 88, 34, 77, 12, 23, 99]
+
+#evens = []
+#odds = []
+
+#for index in range(len(numbers)):
+    #if numbers[index] % 2 == 0:
+        #evens.append(numbers[index])
+    #else:
+        #odds.append(numbers[index])
+
+#print("Evens:", evens)
+#print("Odds:", odds)
+
+
+
+
+#2) შექმენით სახელების სია სადაც გექნებათ მინიმუმ 10 სახელი, დაბეჭდეთ ამ სიიდან მხოლოდ ის სახელები რომლების ინდექსებიც არის ლუწი
+
+#evens = []
+
+#names = ["nika", "lika", "luka", "gio", "saba","sandro", "lile", "lasha", "elene", "zura" ]
+
+
+#for index in range(len(names)):
+    #if names[index] % 2 == 0:
+        #evens.append(names[index])
+ 
+
+
+
+#3) შექმენით სია სადაც გექნებათ 10 რიცხვი, თქვენი დავალებაა გაიგოთ ამ სიაში ყველა ლუწი და კენტი რიცხვიოს ჯამი და დაბეჭდოთ
+    
+#numbers = [45, 67, 34, 66, 88, 34, 77, 12, 23, 99]
+
+#evens = []
+#odds = []
+
+
+#for index in range(len(numbers)):
+    #if numbers[index] % 2 == 0:
+        #evens.append(numbers[index])
+    #else:
+        #odds.append(numbers[index])
+
+
+
+
