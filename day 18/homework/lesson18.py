a58f218d3b82ed3de4c6a4d0d8b4485e3d909673
nika19914 <nikakakhadze48@gmail.com>
2024-11-10 00:43:08 +0400

    nika

---
+++ day 18/homework/lesson18.py
@@ -0,0 +1,49 @@
+#2)  შექმენით string-ების სია, გადაუარეთ მას for loop-ით და დაბეჭდეთ სიაში არსებული ყველა სიტყვის სიმბოლოთა რაოდენობა
+
+#car_name = ["bmw", "mercedes", "nissan", "opel"]
+
+#for i in car_name:
+    #print(len(i))
+
+
+
+# 3) შექმენით რიცხვების სია, while loop-ის გამოყენებით გაიგეთ ამ სიაში ყველა ლუწი რიცხვის ჯამი და დაპრინტეთ
+
+#number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#count = 2
+#sum = 0
+
+#while count <= number:
+    #if count%2 == 0:
+      #sum = sum + count
+    #count = count + 1  
+#print(sum)
+
+#4) შექმენით სახელების სია, გადაუარეთ მას for loop-ით და დაბეჭდეთ ამ სიიდან მხოლოდ ის სახელები, რომლებიც იწყებიან "A"-ზე
+
+#5) შექმენით რიცხვების სია, სადაც გექნებათ დუბლიკატები. გადაუარეთ მას for loop-ით და დაბეჭდეთ მხოლოდ ისეთი რცხვების ჯამი, რომლებიც არ მეორდება სიაში
+
+#numbers = [4, 4, 7, 7, 3, 3]
+
+#no_duplicates = []
+
+#for char in numbers:
+     #if no_duplicates.count(char) == 0:
+         #no_duplicates.append(char)
+
+
+#numbers= [4, 4, 7, 7, 3, 3]
+
+#no_duplicates = []
+
+#for char in numbers:
+     #if char not in no_duplicates:
+         #no_duplicates.append(char)
+
+#print(no_duplicates)
+
+
+#6) შექმენით პროგრამა, რომელიც მომხარებელს შემოატანინებს რიცხვს და დაბეჭდავს 1-დან შემოტანილ რიცხვამდე ყველა მარტივ რიცხვს
+
+
+
