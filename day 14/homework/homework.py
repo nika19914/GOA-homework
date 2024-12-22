787106aa4869f01dfd3f6e73f4f34d10116bfda9
nika19914 <nikakakhadze48@gmail.com>
2024-12-22 19:14:41 +0400

    nika

---
+++ day 14/homework/homework.py
@@ -0,0 +1,39 @@
+#numbers = [1, 2, 0, 1, 32, 1, 30, 1, 1, 21, 1, 1, 1]
+
+
+#count = 0
+
+
+#for number in numbers:
+    #if number == 1:  
+        #count += 1  
+
+#print(f"რიცხვი 1 სიაში შედის {count}-ჯერ.")
+
+
+#short_words = []
+#long_words = []
+
+
+#for _ in range(5):
+    #word = input("შეიყვანეთ სიტყვა: ")
+    ##if len(word) <= 5:
+        #short_words.append(word)
+    #else:
+        #long_words.append(word)
+
+
+#print("პირველი სია (სიგრძე ≤ 5):", short_words)
+#print("მეორე სია (სიგრძე > 5):", long_words)
+
+
+
+#even_numbers = []
+
+
+##for number in range(1, 101):
+   # if number % 2 == 0:  
+       # even_numbers.append(number)
+
+
+#print("ლუწი რიცხვები 1-დან 100-მდე:", even_numbers)
---
+++ day 30/homework/homework.py
@@ -0,0 +1,37 @@
+#def manual_pop(lst, index):   
+   # if index < 0 or index >= len(lst):
+     #   raise IndexError("ინდექსი სიის ფარგლებს გარეთაა.")
+    
+    
+   # new_list = lst[:index] + lst[index+1:]
+    
+   ## return new_list
+
+
+##my_list = [10, 20, 30, 40, 50]
+#index_to_remove = 2
+#updated_list = manual_pop(my_list, index_to_remove)
+#print("განახლებული სია:", updated_list)
+
+
+
+#def manual_len(input_data):
+    
+   # length = 0
+    
+ 
+   # for _ in input_data:
+      #  length += 1
+    
+   # return length
+#
+
+#my_string = "გამარჯობა"
+#my_list = [1, 2, 3, 4, 5]
+
+
+#string_length = manual_len(my_string)
+#list_length = manual_len(my_list)
+
+#print("სტრინგის სიგრძე:", string_length)  
+#print("სიის სიგრძე:", list_length)        
